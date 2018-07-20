951bcd65eaf900631141d8023059be42c3f5e23c
Malcolm D. Forbes <malcolm.forbes500@gmail.com>
2018-07-20 16:28:11 -0400

    Now with both types of boundary conditions.

--- include/boundary_values.h
+++ include/boundary_values.h
@@ -2,15 +2,21 @@
 #define BOUNDARYVALUES_H
 
 #include <deal.II/base/function.h>
+#include <deal.II/base/tensor.h>
 
-//Class handeling essential boundary values of system
+//Class handeling natural boundary values of system
 template <int dim>
 class BoundaryValues : public dealii::Function<dim>
 {
+protected:
+  static const double displacement_value  =   5;
 public:
   BoundaryValues () : dealii::Function<dim>() {}    //constructor
 
   virtual double value (const dealii::Point<dim>   &p,
                         const unsigned int  component = 0) const;
+
+  virtual Tensor<1,dim> gradient (const dealii::Point<dim> &p,
+                        const unsigned int component = 0) const;
 };
 #endif
