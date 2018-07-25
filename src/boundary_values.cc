0c72fef4a0adceed8df372b68d132e9dbfd70eb4
Malcolm D. Forbes <malcolm.forbes500@gmail.com>
2018-07-25 16:08:54 -0400

    I improperly merged some content.

--- src/boundary_values.cc
+++ src/boundary_values.cc
@@ -4,20 +4,7 @@ template <int dim>
 double BoundaryValues<dim>::value (const dealii::Point<dim> &p,
                                    const unsigned int /*component*/) const
 {
-  return this->displacement_value;
-}
-
-template <int dim>
-double BoundaryValues<dim>::gradient (const dealii::Point<dim> &p,
-                                    const unsigned int /*components*/) const
-{
-  //Should have numerical gradient computation here.
-  //Assume each element has one gradient across it (gradient averaging) and use
-  //that for computation. Best method? Dunno. But it is all I can come up with
-  //right now. Possible to modify this technique later.
-
-  //Use a domain-averaging gradient (in 2D area- and in 3D volume-averaging)
-  return 0.;
+  return p.square();
 }
 
 //template <int dim>
