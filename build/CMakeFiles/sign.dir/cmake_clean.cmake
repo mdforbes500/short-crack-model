31710fd8cec9de4ec4422646707f7f44bed3032f
Malcolm D. Forbes <malcolm.forbes500@gmail.com>
2018-07-25 16:09:49 -0400

    now cleaner.

--- build/CMakeFiles/sign.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sign"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sign.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
