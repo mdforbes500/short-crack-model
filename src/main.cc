11664e28fbf004fbddc35781dff64a4512856db0
Malcolm D. Forbes <malcolm.forbes500@gmail.com>
2018-08-22 13:04:17 -0400

    Now with the list option

--- src/main.cc
+++ src/main.cc
@@ -3,10 +3,11 @@
 
 int main(int argc, char* argv[])
 {
+  // Help page
   if (std::strcmp(argv[1], "--help") == 0)
   {
     std::cout << "SCMOD Usage:" << std::endl << std::endl
-              << argv[0] << " [options]" << std::endl << std::endl
+              << "            "<< argv[0] << " [options]" << std::endl << std::endl
               << "Options:" << std::endl << std::endl
               << "--help                  Prints usage information and exits" << std::endl
               << "--run [example | path]  Builds and runs an example, then exits" << std::endl
@@ -15,29 +16,66 @@ int main(int argc, char* argv[])
               << "--view [example | path] Displays results of example, then exits" << std::endl
               << std::endl
               << "Flags:" << std::endl << std::endl
-              << "-T                      Thirdparty packages" << std::endl
-              << "-E                      Examples" <<std::endl
+              << "-T             Thirdparty packages" << std::endl
+              << "-E             Examples" <<std::endl
               << std::endl;
   }
   else if (std::strcmp(argv[1], "--run") == 0)
   {
     std::cout << "running example" << std::endl;
   }
+
+  // For listing thirdparty packages and examples
   else if (std::strcmp(argv[1], "--list") == 0)
   {
-    std::cout << "list example" << std::endl;
+    // List all thirdparty packages
+    if (std::strcmp(argv[2], "-T") == 0)
+    {
+      std::cout << "Thirdparty Packages:" << std::endl << std::endl
+                << "  deal.II" << std::endl
+                << "  Trilinos" << std::endl
+                << "  PetSc" << std::endl
+                << "  p4est" << std::endl
+                << "  Metis" << std::endl
+                << "  PRISM packages" << std::endl
+                << "  Paraview" << std::endl
+                << "  Trelis" << std::endl
+                << "  VisIt" << std::endl
+                << std::endl;
+    }
+
+    // List all Examples
+    else if (std::strcmp(argv[2], "-E") == 0)
+    {
+      std::cout << "list example" << std::endl;
+    }
+
+    // For invalid flags
+    else
+    {
+      std::cerr << "Usage: " << argv[0] << " --list [-T | -E]" << std::endl
+                << "Please specify a relevant flag..."
+                << std::endl;
+      return 2;
+    }
   }
+
+  // For installing thirdparty packages
   else if (std::strcmp(argv[1], "install") == 0)
   {
     std::cout << "installing thirdparty packages" << std::endl;
   }
+
+  // For viewing results
   else if (std::strcmp(argv[1], "--view") == 0)
   {
     std::cout << "view" << std::endl;
   }
+
+  // Usage errors
   else
   {
-    std::cerr << "Usage: " << argv[0] << " [--help] [--run example] [--list] [install --thirdparty] [--view result]"
+    std::cerr << "Usage: " << argv[0] << " [--help] [--run example | path] [--list -T | -E] [install -T | -E path] [--view example | path]"
               << std::endl;
     return 1;
   }
