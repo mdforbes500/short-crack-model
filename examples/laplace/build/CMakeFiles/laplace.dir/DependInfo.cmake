c76323530d7c679d1d4202e630815ae7e8c3fbcb
Malcolm D. Forbes <malcolm.forbes500@gmail.com>
2018-08-22 11:14:39 -0400

    Better gitignore and corrected cmakelists

---
+++ examples/laplace/build/CMakeFiles/laplace.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/duncanforbes/Documents/dealii-workspace/short-crack-model/examples/laplace/src/main.cc" "/Users/duncanforbes/Documents/dealii-workspace/short-crack-model/examples/laplace/build/CMakeFiles/laplace.dir/src/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DEBUG"
+  "TBB_DO_ASSERT=1"
+  "TBB_USE_DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/local/include"
+  "/usr/local/include/deal.II/bundled"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ examples/laplace/build/CMakeFiles/laplace.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laplace.dir/src/main.cc.o"
+  "laplace.pdb"
+  "laplace"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laplace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
