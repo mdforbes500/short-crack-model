201e49cb83173a66951121025e4d758245dd648e
Malcolm D. Forbes <malcolm.forbes500@gmail.com>
2018-08-22 12:39:37 -0400

    Revised help implementation to reflect usability guidelines

--- src/main.cc
+++ src/main.cc
@@ -8,11 +8,15 @@ int main(int argc, char* argv[])
     std::cout << "SCMOD Usage:" << std::endl << std::endl
               << argv[0] << " [options]" << std::endl << std::endl
               << "Options:" << std::endl << std::endl
-              << "--help           Prints usage information and exits" << std::endl
-              << "--run [example]  Builds and runs an example, then exits" << std::endl
-              << "--list           Lists all examples and exits" << std::endl
-              << "install -[tp]    Installs a thirdparty package, then exits" << std::endl
-              << "--view [example] Displays results of example, then exits" << std::endl
+              << "--help                  Prints usage information and exits" << std::endl
+              << "--run [example | path]  Builds and runs an example, then exits" << std::endl
+              << "--list [flags]          Lists all examples and exits" << std::endl
+              << "install [flags] [path]  Installs a thirdparty package, then exits" << std::endl
+              << "--view [example | path] Displays results of example, then exits" << std::endl
+              << std::endl
+              << "Flags:" << std::endl << std::endl
+              << "-T                      Thirdparty packages" << std::endl
+              << "-E                      Examples" <<std::endl
               << std::endl;
   }
   else if (std::strcmp(argv[1], "--run") == 0)
