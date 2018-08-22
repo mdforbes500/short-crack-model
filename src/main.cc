c437372bf8fc71b5a75d526c8daf13cffc1a9ef6
Malcolm D. Forbes <malcolm.forbes500@gmail.com>
2018-08-22 12:29:43 -0400

    Basic help page for app

--- src/main.cc
+++ src/main.cc
@@ -5,31 +5,37 @@ int main(int argc, char* argv[])
 {
   if (std::strcmp(argv[1], "--help") == 0)
   {
-    std::cout << "help page" << std::endl;
+    std::cout << "SCMOD Usage:" << std::endl << std::endl
+              << argv[0] << " [options]" << std::endl << std::endl
+              << "Options:" << std::endl << std::endl
+              << "--help           Prints usage information and exits" << std::endl
+              << "--run [example]  Builds and runs an example, then exits" << std::endl
+              << "--list           Lists all examples and exits" << std::endl
+              << "install -[tp]    Installs a thirdparty package, then exits" << std::endl
+              << "--view [example] Displays results of example, then exits" << std::endl
+              << std::endl;
   }
   else if (std::strcmp(argv[1], "--run") == 0)
   {
-    std::cout << "running example" << std:endl;
+    std::cout << "running example" << std::endl;
   }
   else if (std::strcmp(argv[1], "--list") == 0)
   {
-    std::cout << "list example" << std:endl;
+    std::cout << "list example" << std::endl;
   }
   else if (std::strcmp(argv[1], "install") == 0)
   {
-    std::cout << "installing thirdparty packages" << std:endl;
+    std::cout << "installing thirdparty packages" << std::endl;
   }
   else if (std::strcmp(argv[1], "--view") == 0)
   {
-    std::cout << "view" << std:endl;
+    std::cout << "view" << std::endl;
   }
   else
   {
     std::cerr << "Usage: " << argv[0] << " [--help] [--run example] [--list] [install --thirdparty] [--view result]"
               << std::endl;
-
     return 1;
   }
-
   return 0;
 }
