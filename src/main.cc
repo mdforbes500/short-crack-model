57529966fa9c92d60214b9ca5e06fe922abfa088
Malcolm D. Forbes <malcolm.forbes500@gmail.com>
2018-08-22 12:04:51 -0400

    Now with a menu

--- src/main.cc
+++ src/main.cc
@@ -3,16 +3,32 @@
 
 int main(int argc, char* argv[])
 {
-  if (argc > 3)
+  if (std::strcmp(argv[1], "--help") == 0)
+  {
+    std::cout << "help page" << std::endl;
+  }
+  else if (std::strcmp(argv[1], "--run") == 0)
+  {
+    std::cout << "running example" << std:endl;
+  }
+  else if (std::strcmp(argv[1], "--list") == 0)
+  {
+    std::cout << "list example" << std:endl;
+  }
+  else if (std::strcmp(argv[1], "install") == 0)
+  {
+    std::cout << "installing thirdparty packages" << std:endl;
+  }
+  else if (std::strcmp(argv[1], "--view") == 0)
+  {
+    std::cout << "view" << std:endl;
+  }
+  else
   {
     std::cerr << "Usage: " << argv[0] << " [--help] [--run example] [--list] [install --thirdparty] [--view result]"
               << std::endl;
 
     return 1;
-  }
-  if (std::strcmp(argv[1], "--help") == 0)
-  {
-    std::cout << "help page" << std::endl;
   }
 
   return 0;
