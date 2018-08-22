f937326eb385dd83859c3fbe4cd63816efcaff2d
Malcolm D. Forbes <malcolm.forbes500@gmail.com>
2018-08-22 11:54:08 -0400

    Now with a help page

--- src/main.cc
+++ src/main.cc
@@ -3,13 +3,14 @@
 
 int main(int argc, char* argv[])
 {
-  if (argc >= 2)
+  if (argc > 3)
   {
     std::cerr << "Usage: " << argv[0] << " [--help] [--run example] [--list] [install --thirdparty] [--view result]"
               << std::endl;
+
     return 1;
   }
-  else if (std::strcmp(argv[1], "--help"))
+  if (std::strcmp(argv[1], "--help") == 0)
   {
     std::cout << "help page" << std::endl;
   }
